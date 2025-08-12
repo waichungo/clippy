868629d5e223994959eb7ce0099236c1633d34f7
waichungo <waichungojames@gmail.com>
2025-08-12 23:57:14 +0300

    Added profile view model

---
+++ clippy_desktop/viewmodels/ProfileViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clippy.viewmodels
+{
+    public partial class ProfileViewModel : ViewModelBase
+    {
+        [RelayCommand]
+        public void Logout()
+        {
+            App.firebaseClient?.Dispose();
+            App.firebaseClient = null;
+            MainWindow.context.CurrentUser = null;
+            MainWindow.context.CurrentPage = Page.LOGIN;
+        }
+
+    }
+}
