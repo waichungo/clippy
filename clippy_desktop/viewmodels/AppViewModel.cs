85b330910368f8deb3b8f2a14107518018b2bbe8
waichungo <waichungojames@gmail.com>
2025-08-12 23:54:54 +0300

    Added Application view model

---
+++ clippy_desktop/viewmodels/AppViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clippy.viewmodels
+{
+   
+    public partial class AppViewModel : ViewModelBase
+    {
+      
+        [RelayCommand]
+        public async Task ShowWindow()
+        {
+            App.DisplayMainWindow();
+
+        }
+        [RelayCommand]
+        public void Exit()
+        {
+            var app = (App?)App.Current;
+            app?.CloseApp();
+        }
+    }
+}
