47524eedc2a07b5b5b5ac2af2f2227d69bd25188
waichungo <waichungojames@gmail.com>
2025-08-12 23:54:32 +0300

    Added AppSettings helper

---
+++ clippy_desktop/viewmodels/AppSettings.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clippy.viewmodels
+{
+    public partial class AppSettings:ViewModelBase
+    {
+        [ObservableProperty]
+        private bool _captureClips = true; 
+        [ObservableProperty]
+        private bool _captureImageClips = true; 
+        [ObservableProperty]
+        private bool _saveLoginInfo = true;
+        [ObservableProperty]
+        private bool _launchAtStartUp = true; 
+        [ObservableProperty]
+        private Device? lastSelectedDevice = null;
+        [ObservableProperty]
+        private Dictionary<string,string> devicePositions = new();
+    }
+}
