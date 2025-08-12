b2d384244d73e299660660ccb71e605aef22d46f
waichungo <waichungojames@gmail.com>
2025-08-12 23:53:28 +0300

    Added page to control converter

---
+++ clippy_desktop/converters/PageToControlConverter.cs
@@ -0,0 +1,52 @@
+﻿using Avalonia.Data.Converters;
+using Clippy.viewmodels;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clippy.converters
+{
+    public class PageToControlConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is Page page)
+            {
+                if (page == Page.HOME || page == Page.SETTINGS)
+                {
+                    return new Home();
+                }
+                if (page == Page.LOGIN)
+                {
+                    if (MainWindow.context.CurrentUser != null)
+                    {
+                        return new ProfilePage();
+                    }
+                    return new LoginPage();
+                }
+                if (page == Page.PROFILE)
+                {
+                    if (MainWindow.context.CurrentUser == null)
+                    {
+                        return new LoginPage();
+                    }
+                    return new ProfilePage();
+                }
+                if (page == Page.REGISTER)
+                {
+                    return new RegisterPage();
+                }
+
+            }
+            return null;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
